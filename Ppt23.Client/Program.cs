453a28a772db0322660b7fe0b73c4e99b43116c7
David Švihálek <svihadav@cvut.cz>
2023-04-18 09:59:14 +0200

    program.cs fix 09

--- Ppt23.Client/Program.cs
+++ Ppt23.Client/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:2222") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["AllowedOrigins"]) });
 
 await builder.Build().RunAsync();
