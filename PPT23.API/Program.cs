e2a81fde530619a3f8e40b4f377ba602c1edf88f
Teg3z <tegez@gmail.com>
2023-05-28 14:34:19 +0200

    OK?

--- PPT23.API/Program.cs
+++ PPT23.API/Program.cs
@@ -107,24 +107,17 @@ app.MapPut("/vybaveni/{Id}", (VybaveniVM prichoziModel) =>
     item.Cena = prichoziModel.Cena;
     return Results.Ok();
 });
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast")
-//.WithOpenApi();
+
+
+using var appContext = app.Services.CreateScope().ServiceProvider.GetRequiredService<PptDbContext>();
+try
+{
+    appContext.Database.Migrate();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception during db migration {ex.Message}");
+    //throw;
+}
 
 app.Run();
-
-//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-//{
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-//}
